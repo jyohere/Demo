771f297aa5d47c9110107d575bc70fc015bc4777
Jyothish Damodaran <jyohere@gmail.com>
2015-09-29 13:08:05 +0530

    First commit

---
+++ DemoProject/DemoProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoProject
+//
+//  Created by HM on 9/29/15.
+//  Copyright (c) 2015 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
